ceb3f77c56404f1c1837917f1b7afb9ab9b2f0ff
Amardeep Singh <amardeeps5201@gmail.com>
2021-07-20 03:39:59 -0400

    Create delete-empty.sh

---
+++ delete-empty.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+date '+keyreg-teal-test start %Y%m%d_%H%M%S'
+
+set -e
+set -x
+set -o pipefail
+export SHELLOPTS
+
+WALLET=$1
+
+# Directory of this bash program
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+gcmd="../../goal -d ../test/Primary"
+
+ACCOUNT=$(${gcmd} account list|awk '{ print $3 }'|head -n 1)
+
+# pass in the escrow account to accounts array to check if it is empty
+${gcmd} app delete --app-id 1 --from $ACCOUNT  --app-account=F4HJHVIPILZN3BISEVKXL4NSASZB4LRB25H4WCSEENSPCJ5DYW6CKUVZOA 
