38bcce668c1cfe4708d4f37811b0e3df782b5b37
Amardeep Singh <amardeeps5201@gmail.com>
2021-07-21 19:52:03 -0400

    Create master-fund-john.js

---
+++ Deployment_Algo_Builder/scripts/transfer/master-fund-john.js
@@ -0,0 +1,12 @@
+const { executeTransaction } = require('@algo-builder/algob');
+const { mkParam } = require('./common');
+
+async function run (runtimeEnv, deployer) {
+  const masterAccount = deployer.accountsByName.get('master-account');
+  const john = deployer.accountsByName.get('john');
+
+  // fund John account with 1 Algo
+  await executeTransaction(deployer, mkParam(masterAccount, john.addr, 1e6, { note: 'ALGO PAID' }));
+}
+
+module.exports = { default: run };
