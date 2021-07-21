1b3284d318aa270d48fe0696cefe88a8aa477818
Amardeep Singh <amardeeps5201@gmail.com>
2021-07-21 19:52:37 -0400

    Create nft_transfer_shoe-john.js

---
+++ Deployment_Algo_Builder/scripts/transfer/nft_transfer_shoe-john.js
@@ -0,0 +1,23 @@
+const { executeTransaction, balanceOf } = require('@algo-builder/algob');
+const { types } = require('@algo-builder/runtime');
+
+async function run (runtimeEnv, deployer) {
+  const teslaAssetID = deployer.asa.get('tesla').assetIndex;
+
+  const john = deployer.accountsByName.get('john');
+  const elon = deployer.accountsByName.get('elon-musk');
+
+  await executeTransaction(deployer, {
+    type: types.TransactionType.TransferAsset,
+    sign: types.SignType.SecretKey,
+    fromAccount: elon,
+    toAccountAddr: john.addr,
+    amount: 184467440737095516n, // use bigint for large transfer amount
+    assetID: 'tesla', // passing asa name is also supported
+    payFlags: {}
+  });
+
+  await balanceOf(deployer, john.addr, teslaAssetID);
+}
+
+module.exports = { default: run };
